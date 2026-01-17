5722ca76cb7b53ef2a7e55b668d9cc859520785f
xueqingaa <qf.xue1990@gmail.com>
2026-01-17 23:07:18 +0800

    get-links.js

--- functions/api/get-links.js
+++ functions/api/get-links.js
@@ -2,11 +2,12 @@ export async function onRequestGet(context) {
   const kv = context.env.SUB_KV;
 
   const data = {
+    titleText: await kv.get("titleText") || "XUE CF代理",
     sub1: await kv.get("sub1") || "123",
     sub2: await kv.get("sub2") || "456",
     notice: await kv.get("notice") || "马到成功",
-    bgUrl: await kv.get("bgUrl") || "",  // 空则用默认渐变
-    userPassword: await kv.get("userPassword") || "88888888"  // 访问密码，默认八个8
+    bgUrl: await kv.get("bgUrl") || "",
+    userPassword: await kv.get("userPassword") || "88888888"
   };
 
   return new Response(JSON.stringify(data), {
