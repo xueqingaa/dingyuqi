8bd4254b07b8251f560e857fe79e4ab6a27b396e
xueqingaa <qf.xue1990@gmail.com>
2026-01-18 23:50:05 +0800

    get-links.js

--- functions/api/get-links.js
+++ functions/api/get-links.js
@@ -3,11 +3,11 @@ export async function onRequestGet(context) {
 
   const data = {
     titleText: await kv.get("titleText") || "XUE CF代理",
-    sub1: await kv.get("sub1") || "123",
-    sub2: await kv.get("sub2") || "456",
-    notice: await kv.get("notice") || "马到成功",
+    sub1: await kv.get("sub1") || "",
+    sub2: await kv.get("sub2") || "",
+    sub3: await kv.get("sub3") || "", 
+    notice: await kv.get("notice") || "",
     bgUrl: await kv.get("bgUrl") || "",
-    // 关键修复：增加下面这一行，从数据库读取音乐链接
     musicUrl: await kv.get("musicUrl") || "",
     userPassword: await kv.get("userPassword") || "88888888"
   };
