c764049f7e85f5939d903074db45a0db07dc769e
xueqingaa <qf.xue1990@gmail.com>
2026-01-18 23:37:28 +0800

    get-links.js

--- functions/api/get-links.js
+++ functions/api/get-links.js
@@ -7,10 +7,15 @@ export async function onRequestGet(context) {
     sub2: await kv.get("sub2") || "456",
     notice: await kv.get("notice") || "马到成功",
     bgUrl: await kv.get("bgUrl") || "",
+    // 关键修复：增加下面这一行，从数据库读取音乐链接
+    musicUrl: await kv.get("musicUrl") || "",
     userPassword: await kv.get("userPassword") || "88888888"
   };
 
   return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" }
+    headers: { 
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache" 
+    }
   });
 }
